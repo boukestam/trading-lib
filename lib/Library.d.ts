4f010dc767f2a60e0da54b00f1f04d603c15d8d0
Bouke Stam <bouke.stam@gmail.com>
2021-06-06 11:35:02 +0200

    Ema fix

--- lib/Library.d.ts
+++ lib/Library.d.ts
@@ -1,11 +1,13 @@
 import { Candles } from "./Candles";
+import { OHLC } from "./OHLC";
 export declare const Library: {
+    trueRange: (current: OHLC, previous: OHLC) => number;
     ma: (candles: Candles, length: number, source?: 'low' | 'high' | 'close' | 'open', offset?: number) => number;
     ema: (candles: Candles, length: number, source?: 'low' | 'high' | 'close' | 'open', offset?: number) => number;
     atr: (candles: Candles, length: number, offset?: number) => number;
     lowest: (candles: Candles, length: number, source?: 'low' | 'high' | 'close' | 'open', offset?: number) => number;
     highest: (candles: Candles, length: number, source?: 'low' | 'high' | 'close' | 'open', offset?: number) => number;
-    adx: (candles: Candles, length: number, offset?: number) => number;
+    dmi: (candles: Candles, length: number, offset?: number) => number[];
     roc: (candles: Candles, length: number, offset?: number) => number;
     macd: (candles: Candles, smoothing?: number, offset?: number) => number;
 };
--- src/Library.ts
+++ src/Library.ts
@@ -1,6 +1,14 @@
 import { Candles } from "./Candles";
+import { OHLC } from "./OHLC";
 
 export const Library = {
+  trueRange: (current: OHLC, previous: OHLC): number => {
+    return Math.max(
+      current.high - current.low,
+      Math.abs(current.high - previous.close),
+      Math.abs(current.low - previous.close)
+    );
+  },
   ma: (candles: Candles, length: number, source: 'low' | 'high' | 'close' | 'open' = 'close', offset: number = 0): number => {
     let total = 0;
 
@@ -12,9 +20,9 @@ export const Library = {
   },
   ema: (candles: Candles, length: number, source: 'low' | 'high' | 'close' | 'open' = 'close', offset: number = 0): number => {
     const k = 2 / (length + 1);
-    let ema = candles.get(Math.max(candles.length - offset - length, 0))[source];
+    let ema = Library.ma(candles, length, source, length + offset);
 
-    for (let i = candles.length - offset - length + 1; i >= 0 && i < candles.length; i++) {
+    for (let i = candles.length - offset - length; i < candles.length - offset; i++) {
       ema = (candles.get(i)[source] * k) + (ema * (1 - k));
     }
 
@@ -22,23 +30,20 @@ export const Library = {
   },
   atr: (candles: Candles, length: number, offset: number = 0): number => {
     let total = 0;
+    let count = 0;
 
-    for (let i = candles.length - length - offset; i < candles.length - offset; i++) {
-      const candle = candles.get(i);
-      total += Math.max(
-        candle.high - candle.low,
-        Math.abs(candle.high - candle.close),
-        Math.abs(candle.low - candle.close)
-      );
+    for (let i = Math.max(0, candles.length - length - offset); i < candles.length - offset; i++) {
+      total += Library.trueRange(candles.get(i), candles.get(i - 1));
+      count++;
     }
 
-    return total / length;
+    return total / count;
   },
   lowest: (candles: Candles, length: number, source: 'low' | 'high' | 'close' | 'open' = 'low', offset: number = 0): number => {
     let low = candles.get(candles.length - length - offset)[source];
 
-    for (let i = candles.length - length - offset + 1; i >= 0 && i < candles.length - offset; i++) {
-      const candle = candles.get(i);
+    for (let i = 1; i < length; i++) {
+      const candle = candles.get(Math.max(candles.length - length - offset + i, 0));
       if (candle[source] < low) low = candle[source];
     }
 
@@ -47,27 +52,46 @@ export const Library = {
   highest: (candles: Candles, length: number, source: 'low' | 'high' | 'close' | 'open' = 'high', offset: number = 0): number => {
     let high = candles.get(Math.max(candles.length - length - offset, 0))[source];
 
-    for (let i = candles.length - length - offset + 1; i >= 0 && i < candles.length - offset; i++) {
-      const candle = candles.get(i);
+    for (let i = 1; i < length; i++) {
+      const candle = candles.get(Math.max(candles.length - length - offset + i, 0));
       if (candle[source] > high) high = candle[source];
     }
 
     return high;
   },
-  adx: (candles: Candles, length: number, offset: number = 0): number => {
-    
+  dmi: (candles: Candles, length: number, offset: number = 0): number[] => {
+    let plusDM = 0;
+    let minusDM = 0;
+    let tr = 0;
 
-    for (let i = candles.length - length - offset + 1; i < candles.length - offset; i++) {
+    for (let i = candles.length - length - length - offset; i < candles.length - length - offset; i++) {
       const candle = candles.get(i);
       const prevCandle = candles.get(i - 1);
 
-      const plusDM = candle.high - prevCandle.high;
-      const minusDM = prevCandle.low - candle.low;
-      
-      
+      plusDM += Math.max(0, candle.high - prevCandle.high);
+      minusDM += Math.max(0, prevCandle.low - candle.low);
+      tr += Library.trueRange(candle, prevCandle);
+    }
+    
+    let plusDI = 0;
+    let minusDI = 0;
+    let dx = 0;
+
+    for (let i = candles.length - length - offset; i < candles.length - offset; i++) {
+      const candle = candles.get(i);
+      const prevCandle = candles.get(i - 1);
+
+      plusDM = plusDM - (plusDM / 14) + Math.max(0, candle.high - prevCandle.high);
+      minusDM = minusDM - (minusDM / 14) + Math.max(0, prevCandle.low - candle.low);
+      tr = tr - (tr / 14) + Library.trueRange(candle, prevCandle);
+
+      plusDI = (plusDM / tr) * 100;
+      minusDI = (minusDM / tr) * 100;
+
+      dx += (plusDI - minusDI) / (plusDI + minusDI) * 100;
     }
 
-    return 0;
+    return [plusDI, minusDI, dx / length];
   },
   roc: (candles: Candles, length: number, offset: number = 0): number => {
     const current = candles.get(Math.max(candles.length - 1 - offset, 0)).close;
